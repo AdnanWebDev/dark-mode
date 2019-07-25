0405bcd06aac08b2022bd5dc372667327948e910
Adnan <adnan.chowdhury@outlook.com>
2019-07-25 17:12:08 -0500

    Built Dark Mode Component.

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,20 @@
+import React, { useEffect }from 'react';
+
+import useLocalStorage from './useLocalStorage'
+
+// useEffect(()=>{
+//     if(!something)
+// },[])
+
+const useDarkMode = () => {
+    
+    const [someValue, setSomeValue] = useLocalStorage('mode')
+    useEffect(()=>{
+        if(someValue === true){
+            document.body.classList.toggle('dark.mode')
+        }
+    },[someValue])
+    return [someValue,setSomeValue];
+}
+ 
+export default useDarkMode;
